8f63521faeedfb02c5982eadf4d91e0d1b4873fd
Andre Mapa <andreomapa01@gamil.com>
2022-07-05 10:07:59 -0300

    First Commit - Base Project and connection with database done

---
+++ src/main/java/com/andremapa/userVehicleController/UserVehicleControllerApplication.java
@@ -0,0 +1,12 @@
+package com.andremapa.userVehicleController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserVehicleControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserVehicleControllerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/andremapa/userVehicleController/UserVehicleControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andremapa.userVehicleController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserVehicleControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
